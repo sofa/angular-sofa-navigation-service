8cc95fce97e986764a469e5bab29fc561740258f
Pascal Precht <pascal.precht@googlemail.com>
2015-02-06 15:43:01 +0100

    chore(sofa.navigationService): release 0.1.0

---
+++ test/sofaNavigationService.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('sofa.navigationService', function () {
+
+    beforeEach(module('sofa.navigationService'));
+
+    it('should run tests', function () {
+        expect(true).toBe(true);
+    });
+});
