f42ee6a1857f1a21a0faf1b8554276a43164cf14
Robin Thrift <thrift.d.robin@googlemail.com>
2015-02-18 14:05:06 +0100

    chore: added tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,22 @@
 require('shared-sofa-component-tasks')(require('gulp'), {
     pkg: require('./package.json'),
-    baseDir: __dirname
+    baseDir: __dirname,
+    testDependencyFiles: [
+        'node_modules/sofa-couch-service/dist/sofaCouchService.js',
+        'node_modules/sofa-couch-service/dist/sofaCouchService.angular.js',
+
+        'node_modules/shared-sofa-component-tasks/node_modules/sofa-core/dist/sofaCore.angular.js',
+
+        'node_modules/sofa-tracking/dist/sofaTracking.js',
+        'node_modules/sofa-tracking/dist/sofaTracking.angular.js',
+
+        'node_modules/sofa-url-parser-service/dist/sofaUrlParserService.js',
+        'node_modules/sofa-url-parser-service/dist/sofaUrlParserService.angular.js',
+
+        'node_modules/sofa-url-construction-service/dist/sofaUrlConstructionService.js',
+        'node_modules/sofa-url-construction-service/dist/sofaUrlConstructionService.angular.js',
+
+        'node_modules/sofa-state-resolver-service/dist/sofaStateResolverService.js',
+        'node_modules/sofa-state-resolver-service/dist/sofaStateResolverService.angular.js',
+    ]
 });
--- test/sofaNavigationService.spec.js
+++ test/sofaNavigationService.spec.js
@@ -2,9 +2,64 @@
 
 describe('sofa.navigationService', function () {
 
+    var $navigationService, $location, stateResolverService;
+
     beforeEach(module('sofa.navigationService'));
+
+    beforeEach(inject(function($injector) {
+        $navigationService = $injector.get('navigationService');
+        $location = $injector.get('$location');
+        stateResolverService = $injector.get('stateResolverService');
+        spyOn($location, 'path').and.callThrough();
+    }));
 
     it('should run tests', function () {
         expect(true).toBe(true);
+    });
+
+    it('should be defined', function () {
+        expect($navigationService).toBeDefined();
+    });
+
+    it('should navigate to the given url', function() {
+        $navigationService.navigateToUrl('test');
+        expect($location.path).toHaveBeenCalled();
+        expect($location.path()).toEqual('/test');
+    });
+
+    it('should navigate to a page url', function() {
+        $navigationService.navigateToContentPage('test');
+        expect($location.path).toHaveBeenCalled();
+        expect($location.path()).toEqual('/pages/test');
+    });
+
+    it('should navigate to the root category', function() {
+        $navigationService.navigateToRootCategory();
+        expect($location.path).toHaveBeenCalled();
+        expect($location.path()).toEqual('/');
+    });
+
+    it('should navigate to the cart', function() {
+        $navigationService.navigateToCart();
+        expect($location.path).toHaveBeenCalled();
+        expect($location.path()).toEqual('/cart');
+    });
+
+    it('should navigate to the checkout page', function() {
+        $navigationService.navigateToCheckout();
+        expect($location.path).toHaveBeenCalled();
+        expect($location.path()).toEqual('/checkout');
+    });
+
+    it('should navigate to the summary page', function() {
+        $navigationService.navigateToSummary('test');
+        expect($location.path).toHaveBeenCalled();
+        expect($location.path()).toEqual('/summary/test');
+    });
+
+    it('should navigate to the shipping costs page', function() {
+        $navigationService.navigateToShippingCostsPage();
+        expect($location.path).toHaveBeenCalled();
+        expect($location.path()).toEqual('/pages/');
     });
 });
